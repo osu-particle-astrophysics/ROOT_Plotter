04e0a88b8e23c0be23a8c4e72c5f30ed26f165af
Dennis H. Calderon <dennis.henry.calderon@gmail.com>
2022-10-17 18:09:14 -0400

    Added env.sh. Source env.sh to set necessary python and root version

---
+++ env.sh
@@ -0,0 +1,7 @@
+module load gnu/4.8.5
+export CC=`which gcc`
+export CXX=`which g++`
+
+source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh 
+
+alias AraSim=/cvmfs/ara.opensciencegrid.org/trunk/centos7/source/AraSim/./AraSim
